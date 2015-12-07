2a37bbafe3154eeae44011a5352052149839f0f5
Jerry Wang <jerryw@gmail.com>
2015-12-07 15:56:44 -0500

    do not run docker make it manual

--- zsh/.zsh/docker.zsh
+++ zsh/.zsh/docker.zsh
@@ -1,11 +1,10 @@
 alias mc='docker-machine'
-alias docker-env='eval "$(mc env dev)"'
+alias docker-env='mc start dev &&  eval "$(mc env dev)"'
 docker_running=$(mc status dev)
 
 if [[ $docker_running == *"Stopped"* ]] 
 then 
-  echo 'starting docker '
-  { mc start dev } &
+  echo 'docker is not running'
 elif [[ $docker_running == *"Running"* ]]
 then 
   eval "$(mc env dev)"
