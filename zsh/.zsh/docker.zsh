a01f779f2c319361237810adb01de43d5e8654ec
Jerry Wang <jerryw@gmail.com>
2016-02-02 15:26:23 -0500

    dont run docker unless i need to

--- zsh/.zsh/docker.zsh
+++ zsh/.zsh/docker.zsh
@@ -2,11 +2,10 @@ alias mc='docker-machine'
 alias docker-env='mc start dev &&  eval "$(mc env dev)"'
 docker_running=$(mc status dev)
 
-if [[ $docker_running == *"Stopped"* ]] 
-then 
+if [[ $docker_running == *"Stopped"* ]]
+then
   echo 'docker is not running'
 elif [[ $docker_running == *"Running"* ]]
-then 
-  echo 'Running eval "$(mc env dev)"'
-  eval "$(mc env dev)"
+then
+  echo 'docker running type eval "$(mc env dev)"'
 fi
