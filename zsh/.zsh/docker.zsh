204f5ef9872bdcac2b2a76956f9aac945d0f1a19
Jerry Wang <jerryw@gmail.com>
2016-04-21 09:57:33 -0400

    adding some docker aliases, tweaking teamocil

--- zsh/.zsh/docker.zsh
+++ zsh/.zsh/docker.zsh
@@ -1,3 +1,5 @@
-if hash docker-machine 2>/dev/null; then
+if hash docker 2>/dev/null; then
   alias mc='docker-machine'
+  alias dl='docker ps -l -q'
+  alias dbash='docker exec -it $(dl) /bin/bash'
 fi
