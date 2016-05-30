67e632f766bf3b4af1eab948997068e10c7eac3c
Jerry Wang <jerryw@gmail.com>
2016-05-30 12:15:50 -0400

    adding docker-clean up alias

--- zsh/.zsh/docker.zsh
+++ zsh/.zsh/docker.zsh
@@ -2,4 +2,5 @@ if hash docker 2>/dev/null; then
   alias mc='docker-machine'
   alias dl='docker ps -l -q'
   alias dbash='docker exec -it $(dl) /bin/bash'
+  alias docker-clean='docker rm $(docker ps -a -q) && docker rmi $(docker images -q) && docker ps -a | cut -c-12 | xargs docker rm'
 fi
