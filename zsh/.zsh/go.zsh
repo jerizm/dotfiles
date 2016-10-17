fccc432a6ac0902207abcd405a55dae6c1f0938f
Jerry Wang <jerryw@gmail.com>
2016-10-17 13:41:28 -0400

    fixing kwm adding khd

--- zsh/.zsh/go.zsh
+++ zsh/.zsh/go.zsh
@@ -3,7 +3,4 @@ if [ "$(uname)" = "Darwin" ]; then
 fi
 
 
-alias go-cover='go test -cover -test.v=true -test.coverprofile=c.out && sed -i -e "s#.*/\(.*\.go\)#\./\\1#" c.out && go tool cover -html c.out -o coverage.html'
-
-
-
+alias go-cover='go test -cover -test.v=true -test.coverprofile=coverage.out && sed -i -e "s#.*/\(.*\.go\)#\./\\1#" coverage.out && go tool cover -html coverage.out -o coverage.html'
