844d62d3538776c7ef5887079e1981b014384982
Jerry Wang <jerryw@gmail.com>
2017-04-03 11:17:57 -0400

    gpg-agent runs as daemon now

--- zsh/.zsh/environment.zsh
+++ zsh/.zsh/environment.zsh
@@ -101,10 +101,3 @@ fi
 if [[ "$OSTYPE" != darwin* ]]; then
   alias gdate=date
 fi
-
-[ -f ~/.gpg-agent-info ] && source ~/.gpg-agent-info
-if [ -S "${GPG_AGENT_INFO%%:*}" ]; then
-  export GPG_AGENT_INFO
-else
-  eval $( gpg-agent --daemon )
-fi
