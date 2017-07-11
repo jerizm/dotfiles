83221a5d779dee9e9f47ca9d393accea16685355
Jerry Wang <jerryw@gmail.com>
2017-07-11 10:07:35 -0400

    adding more aws ecs aliases

--- zsh/.zsh/aws.zsh
+++ zsh/.zsh/aws.zsh
@@ -7,3 +7,15 @@ ecs_service_name() {
 update_service() {
   aws ecs update-service --cluster $(cluster_arn $1) --service $(ecs_service_name $1 $2) $3 $4
 }
+describe_ecs_services() {
+  aws ecs describe-services --services $(ecs_service_name $1 $2) --cluster $(cluster_arn $1)
+}
+list_ecs_tasks() {
+  aws ecs list-tasks --service-name $(ecs_service_name $1 $2) --cluster $(cluster_arn $1) | jq '.taskArns | .[] | sub(".*:task/"; "")' | tr -d '"'
+}
+get_ecs_task() {
+  aws ecs describe-tasks --tasks $(list_ecs_tasks $1 $2) --cluster $(cluster_arn $1) | jq '.tasks[0].taskDefinitionArn' | tr -d '"'
+}
+describe_task_def() {
+  aws ecs describe-task-definition --task-definition $(get_ecs_task $1 $2)
+}
