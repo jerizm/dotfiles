f1919440ed9f6d8dca90c51a1dc171d08a8a2c01
Jerry Wang <jerryw@gmail.com>
2017-08-23 15:59:26 -0400

    invoke pyenv for aws

--- zsh/.zsh/python.zsh
+++ zsh/.zsh/python.zsh
@@ -2,6 +2,10 @@
 if (( $+commands[pyenv] )) ; then
   export PYENV_ROOT="$HOME/.pyenv"
   export PATH="$PYENV_ROOT/bin:$PATH"
+  aws() {
+    eval "$( command pyenv init - )"
+    aws "$@"
+  }
   pyenv() {
     eval "$( command pyenv init - )"
     pyenv "$@"
