0a2b04a05fdb39b463713e8c2a8cf03e9b678a8a
Jerry Wang <jerryw@gmail.com>
2017-08-25 10:33:46 -0400

    blergh

--- zsh/.zsh/python.zsh
+++ zsh/.zsh/python.zsh
@@ -2,10 +2,6 @@
 if (( $+commands[pyenv] )) ; then
   export PYENV_ROOT="$HOME/.pyenv"
   export PATH="$PYENV_ROOT/bin:$PATH"
-  aws() {
-    eval "$( command pyenv init - )"
-    aws "$@"
-  }
   pyenv() {
     eval "$( command pyenv init - )"
     pyenv "$@"
