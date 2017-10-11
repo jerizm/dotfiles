8f44cbcbfd4587c5132c1f2f4b4cde6fc63dd6c6
Jerry Wang <jerryw@gmail.com>
2017-10-11 16:44:32 -0400

    move pyenv to brew script

--- zsh/.zsh/aws.zsh
+++ zsh/.zsh/aws.zsh
@@ -1,14 +1,14 @@
 cluster_arn() {
-  aws ecs list-clusters | grep $1 | tr -d ' "'
+  aws ecs list-clusters | jq '.clusterArns[] | select(. | contains("qa"))' -r
 }
 ecs_service_name() {
-  aws ecs list-services --cluster $(cluster_arn $1) | grep $2 | tr -d ' ",'
+  aws ecs list-services --cluster $(cluster_arn $1) | jq ".serviceArns[] | select(. | contains(\"$2\"))" -r
 }
 update_service() {
   aws ecs update-service --cluster $(cluster_arn $1) --service $(ecs_service_name $1 $2) $3 $4
 }
 describe_ecs_services() {
-  aws ecs describe-services --services $(ecs_service_name $1 $2) --cluster $(cluster_arn $1)
+  aws ecs describe-services --services $(ecs_service_name $1 $2) --cluster $(cluster_arn $1) | jq '.services[] | .events = .events[0]'
 }
 list_ecs_tasks() {
   aws ecs list-tasks --service-name $(ecs_service_name $1 $2) --cluster $(cluster_arn $1) | jq '.taskArns | .[] | sub(".*:task/"; "")' | tr -d '"'
--- zsh/.zsh/tools/brew-upgrade.sh
+++ zsh/.zsh/tools/brew-upgrade.sh
@@ -2,6 +2,10 @@
 
 brew=/usr/local/bin/brew
 
+if command -v pyenv 1>/dev/null 2>&1; then
+  pyenv shell 3.6.2
+fi
+
 $brew update
 $brew upgrade
 $brew cleanup
