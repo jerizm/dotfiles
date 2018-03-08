ccc22116f0aeb17ee376563a709ae451f7c8025f
Jerry Wang <jerryw@gmail.com>
2018-03-08 09:20:10 -0500

    always load nvm

--- zsh/.zsh/nodejs.zsh
+++ zsh/.zsh/nodejs.zsh
@@ -1,20 +1,21 @@
 if [[ -a ~/.nvm/nvm.sh ]]; then
   alias npminstall="rm -rf node_modules && npm install"
 
-  alias node_tests="chokidar 'src/**/*.js' 'test/**/*.js' -c 'npm run lint && if [[ {path} =~ ^test.* ]]; then npm test -- {path}; else npm test; fi;' --throttle 500 --debounce 1000"
+  alias node_tests="chokidar 'src/**/*.js' 'test/**/*.js' -c 'eslint {path} && if [[ {path} =~ ^test.* ]]; then npm test -- {path}; else npm test; fi;' --throttle 500 --debounce 1000"
 
   export NVM_DIR="$HOME/.nvm"
-  nvm() {
-    unset -f nvm
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" --no-use  # This loads nvm
-    nvm "$@"
-  }
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  # nvm() {
+  #   unset -f nvm
+  #   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" --no-use  # This loads nvm
+  #   nvm "$@"
+  # }
 
-  npm() {
-    unset -f npm
-    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" --no-use # This loads nvm
-    npm "$@"
-  }
+  # npm() {
+  #   unset -f npm
+  #   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" --no-use # This loads nvm
+  #   npm "$@"
+  # }
 fi
 
 export NODE_ENV=dev
@@ -22,10 +23,21 @@ export NODE_ENV=dev
 # check for .nvmrc and use
 autoload -U add-zsh-hook
 load-nvmrc() {
-  if [[ -f .nvmrc ]]; then
-    nvm use
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
+  elif [ "$node_version" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
   fi
 }
 add-zsh-hook chpwd load-nvmrc
 load-nvmrc
-
--- zsh/.zsh/python.zsh
+++ zsh/.zsh/python.zsh
@@ -2,9 +2,5 @@
 if (( $+commands[pyenv] )) ; then
   export PYENV_ROOT="$HOME/.pyenv"
   export PATH="$PYENV_ROOT/bin:$PATH"
-  pyenv() {
-    eval "$( command pyenv init - )"
-    pyenv "$@"
-  }
+  eval "$( command pyenv init - )"
 fi
-
