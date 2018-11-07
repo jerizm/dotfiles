3ff0c2b3b7eb4e0c045a15b6c4cde65289945b0a
Jerry Wang <jerryw@gmail.com>
2018-11-07 13:19:08 -0500

    set GOBIN

--- zsh/.zsh/go.zsh
+++ zsh/.zsh/go.zsh
@@ -1,5 +1,6 @@
 if (( $+commands[go] )) ; then
   export GOPATH=$HOME/go
-  export PATH=$PATH:$GOPATH/bin
+  export GOBIN=$GOPATH/bin
+  export PATH=$PATH:$GOBIN
   alias go-cover='go test -cover -test.v=true -test.coverprofile=coverage.out && sed -i -e "s#.*/\(.*\.go\)#\./\\1#" coverage.out && go tool cover -html coverage.out -o coverage.html'
 fi
