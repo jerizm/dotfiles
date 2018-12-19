606c3f6a95803fc91a7403dfda6ad15c586c9170
Jerry Wang <jerryw@gmail.com>
2018-12-19 13:51:47 -0500

    clean up nodejs.zsh

--- zsh/.zsh/nodejs.zsh
+++ zsh/.zsh/nodejs.zsh
@@ -5,17 +5,7 @@ if [[ -a ~/.nvm/nvm.sh ]]; then
 
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  # nvm() {
-  #   unset -f nvm
-  #   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" --no-use  # This loads nvm
-  #   nvm "$@"
-  # }
-
-  # npm() {
-  #   unset -f npm
-  #   [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" --no-use # This loads nvm
-  #   npm "$@"
-  # }
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi
 
 export NODE_ENV=dev
