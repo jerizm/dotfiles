d20e7ce6b1397e4b88c0ec1605e56ecc8f4f0b39
Jerry Wang <jerryw@gmail.com>
2018-12-20 16:42:14 -0500

    kms encrypt/decrypt aliases

--- zsh/.zsh/aws.zsh
+++ zsh/.zsh/aws.zsh
@@ -1,27 +1,6 @@
-cluster_arn() {
-  aws ecs list-clusters | jq '.clusterArns[] | select(. | contains("qa-ecsStack"))' -r
+decrypt_kms() {
+    aws kms decrypt --ciphertext-blob fileb://<(echo "$1" | base64 -D) --output text --query Plaintext | base64 -D
 }
-ecs_service_name() {
-  aws ecs list-services --cluster $(cluster_arn $1) | jq ".serviceArns[] | select(. | contains(\"$2\"))" -r
-}
-update_service() {
-  aws ecs update-service --cluster $(cluster_arn $1) --service $(ecs_service_name $1 $2) $3 $4
-}
-describe_ecs_services() {
-  aws ecs describe-services --services $(ecs_service_name $1 $2) --cluster $(cluster_arn $1) | jq '.services[] | .events = .events[0]'
-}
-list_ecs_tasks() {
-  aws ecs list-tasks --service-name $(ecs_service_name $1 $2) --cluster $(cluster_arn $1) | jq '.taskArns | .[] | sub(".*:task/"; "")' | tr -d '"'
-}
-get_ecs_task() {
-  aws ecs describe-tasks --tasks $(list_ecs_tasks $1 $2) --cluster $(cluster_arn $1) | jq '.tasks[0].taskDefinitionArn' | tr -d '"'
-}
-describe_task_def() {
-  aws ecs describe-task-definition --task-definition $(get_ecs_task $1 $2)
-}
-get_role_policies() {
-  aws iam list-role-policies --role-name $1 --query 'PolicyNames[*]' | tr -d '][",' | while read line; do aws iam get-role-policy --role-name $1 --policy-name $line --query 'PolicyDocument.Statement[*]'; done
-}
-ecr_password() {
-  aws ecr get-login --no-include-email | gsed 's/\(docker login -u AWS -p \)\|\( https.*\)//g'
+encrypt_kms() {
+    aws kms encrypt --plaintext "$1" --key-id "$2"
 }
