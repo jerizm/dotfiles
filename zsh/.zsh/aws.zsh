75d6f438400db1bbd66ca33274d61dfaaa588bf8
Jerry Wang <jerryw@gmail.com>
2019-01-24 13:31:00 -0500

    use gnu version of base64

--- zsh/.zsh/aws.zsh
+++ zsh/.zsh/aws.zsh
@@ -1,5 +1,5 @@
 decrypt_kms() {
-    aws kms decrypt --ciphertext-blob fileb://<(echo "$1" | base64 -D) --output text --query Plaintext | base64 -D
+    aws kms decrypt --ciphertext-blob fileb://<(echo "$1" | base64 -d) --output text --query Plaintext | base64 -d
 }
 encrypt_kms() {
     aws kms encrypt --plaintext "$1" --key-id "$2"
