e0beb7cbbe19e621b92049cd33a1af6d718a9059
Jerry Wang <jerryw@gmail.com>
2019-06-07 12:29:20 -0400

    fix encrypt kms

--- zsh/.zsh/aws.zsh
+++ zsh/.zsh/aws.zsh
@@ -2,6 +2,6 @@ decrypt_kms() {
     aws kms decrypt --ciphertext-blob fileb://<(echo "$1" | base64 -d) --output text --query Plaintext | base64 -d
 }
 encrypt_kms() {
-    aws kms encrypt --plaintext "$1" --key-id "$2"
+    aws kms encrypt --key-id "$1" --plaintext fileb://<(echo -n "$2")
 }
 
