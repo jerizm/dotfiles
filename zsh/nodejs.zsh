8d55a48c11c7323533c2f195b650f134dbcfa9b2
Jerry Wang <jerryw@gmail.com>
2021-05-17 14:31:31 -0400

    find ts files too

--- zsh/nodejs.zsh
+++ zsh/nodejs.zsh
@@ -1,7 +1,7 @@
 if [[ -a ~/.nvm/nvm.sh ]]; then
     alias npminstall="rm -rf node_modules && npm install"
 
-    alias node_tests="chokidar 'handlers/**/*.js' 'lib/**/*.js' 'test/**/*.js' -c 'if [[ {path} =~ ^test.* ]]; then npm test -- {path}; else npm test; fi;' --throttle 500 --debounce 1000"
+    alias node_tests="chokidar 'handlers/**/*.js' 'lib/**/*.js' 'test/**/*.js' 'handlers/**/*.ts' 'lib/**/*.ts' 'test/**/*.ts' -c 'if [[ {path} =~ ^test.* ]]; then npm test -- {path}; else npm test; fi;' --throttle 500 --debounce 1000"
 
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
