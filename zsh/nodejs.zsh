dd47b4e9de8addb61446b58da1a062ded5b121d0
Jerry Wang <jerryw@gmail.com>
2022-04-06 11:00:38 -0400

    add alias

--- zsh/nodejs.zsh
+++ zsh/nodejs.zsh
@@ -4,4 +4,5 @@ if [[ -a /usr/local/bin/fnm ]]; then
     alias node_tests="chokidar 'handlers/**/*.js' 'lib/**/*.js' 'test/**/*.js' 'handlers/**/*.ts' 'lib/**/*.ts' 'test/**/*.ts' -c 'if [[ {path} =~ ^test.* ]]; then npm test -- {path}; else npm test; fi;' --throttle 500 --debounce 1000"
     export NODE_ENV=dev
     eval "$(fnm env)"
+    alias node_global="npm ls -g --depth=0 | grep -oP ' \K(.*)(?=@)'"
 fi
