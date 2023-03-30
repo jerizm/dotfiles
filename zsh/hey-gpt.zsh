45552858b4a57a4ca3d0d50ceb685f85669b8c5f
Jerry Wang <jerryw@gmail.com>
2023-03-30 09:36:44 -0400

    add img_gpt

--- zsh/hey-gpt.zsh
+++ zsh/hey-gpt.zsh
@@ -7,6 +7,21 @@ hey_gpt () {
         "messages": [{"role": "user", "content": "'$1'"}],
         "temperature": 0.7
     }')
-    echo $gpt | jq -r '.choices[0].message.content'
+    echo -E $gpt | jq -r '.choices[0].message.content'
 }
 alias h=hey_gpt
+
+img_gpt () {
+    create_img=$(curl https://api.openai.com/v1/images/generations -s \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $OPENAI_API_KEY" \
+    -d '{
+        "prompt": "'$1'",
+        "n": 1,
+        "size": "1024x1024"
+    }')
+    echo -E $create_img | jq
+    url=$(echo -E $create_img | jq -r '.data[0].url')
+    rand_num=$(shuf -i 1-1000000 -n 1)
+    curl -s $url -o img-"$rand_num".png
+}
