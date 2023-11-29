97ee2cbbead956aa4152a0f3c5296d79424510b5
Jerry Wang <jerryw@gmail.com>
2023-11-29 14:49:10 -0500

    more config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,6 +10,8 @@ vim.api.nvim_set_keymap("n", "<C-n>", ":bprevious<CR>", {
   noremap = true,
   silent = true,
 })
+
+-- tmux navigation
 local nvim_tmux_nav = require("nvim-tmux-navigation")
 
 nvim_tmux_nav.setup({
@@ -22,3 +24,10 @@ vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
 vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
 vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
 vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+
+--  telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
