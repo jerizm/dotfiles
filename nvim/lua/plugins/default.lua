346f324420118ce0aba15e0b0b4a4683127650e4
Jerry Wang <jerryw@gmail.com>
2023-11-29 20:23:53 -0500

    more stuff

--- nvim/lua/plugins/default.lua
+++ nvim/lua/plugins/default.lua
@@ -176,6 +176,7 @@ return {
         "shellcheck",
         "shfmt",
         "flake8",
+        "prettier",
       },
     },
   },
@@ -238,5 +239,38 @@ return {
       vim.g.EasyClipUseYankDefaults = 0
       vim.g.EasyClipUsePasteToggleDefaults = 0
     end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, nls.builtins.formatting.prettier)
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        ["javascript"] = { "prettier" },
+        ["javascriptreact"] = { "prettier" },
+        ["typescript"] = { "prettier" },
+        ["typescriptreact"] = { "prettier" },
+        ["vue"] = { "prettier" },
+        ["css"] = { "prettier" },
+        ["scss"] = { "prettier" },
+        ["less"] = { "prettier" },
+        ["html"] = { "prettier" },
+        ["json"] = { "prettier" },
+        ["jsonc"] = { "prettier" },
+        ["yaml"] = { "prettier" },
+        ["markdown"] = { "prettier" },
+        ["markdown.mdx"] = { "prettier" },
+        ["graphql"] = { "prettier" },
+        ["handlebars"] = { "prettier" },
+      },
+    },
   },
 }
