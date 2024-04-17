f55fd513d25adf20e6e5b90d7ee29592256f9b93
Jerry Wang <jerryw@gmail.com>
2024-04-17 13:49:21 -0400

    fix wifi script

--- sketchybar/items/wifi.sh
+++ sketchybar/items/wifi.sh
@@ -1,8 +1,7 @@
 WIFI=(
-  script="$PLUGIN_DIR/wifi.sh"
+    script="$PLUGIN_DIR/wifi.sh"
 )
 
 sketchybar --add item wifi right
 sketchybar --set wifi "${WIFI[@]}"
 sketchybar --subscribe wifi wifi_change
-
