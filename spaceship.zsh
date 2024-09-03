429aeb81502ab5ef7951a06e796e4e200ca6ff33
Jerry Wang <jerryw@gmail.com>
2024-09-03 12:36:13 -0400

    add spaceship

---
+++ spaceship.zsh
@@ -0,0 +1,9 @@
+# Display time
+SPACESHIP_TIME_SHOW=true
+
+# Add a custom vi-mode section to the prompt
+# See: https://github.com/spaceship-prompt/spaceship-vi-mode
+spaceship add --before char vi_mode
+
+SPACESHIP_AWS_SHOW=true
+
