596c70731c56aebbc0bf64ba25a24b7b5a67b7c7
Jerry Wang <jerryw@gmail.com>
2024-10-09 11:23:59 -0400

    get wifi network correctly

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -1,6 +1,6 @@
 source "$HOME/.config/sketchybar/icons.sh"
 
-INFO="$(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}' | xargs networksetup -getairportnetwork | sed "s/Current Wi-Fi Network: //")"
+INFO="$(ipconfig getsummary en0 | awk -F ' SSID : ' '/ SSID : / {print $2}')"
 if [ "$INFO" = "" ]; then
     sketchybar --set $NAME label="Disconnected" icon="$WIFI_DISCONN_ICON"
 elif [ "$INFO" = "You are not associated with an AirPort network." ]; then
