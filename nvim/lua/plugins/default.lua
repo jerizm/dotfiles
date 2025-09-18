66ebbde6422a5040a8edd629fdd10b1856ae2521
Jerry Wang <jerryw@gmail.com>
2025-09-18 08:26:41 -0400

    fix new lazyvim release

--- nvim/lua/plugins/default.lua
+++ nvim/lua/plugins/default.lua
@@ -125,7 +125,7 @@ return {
 
   -- add any tools you want to have installed below
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = {
       ensure_installed = {
         "eslint_d",
