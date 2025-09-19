a983bab18a9e77f7f5de383c056f14f338983507
Jerry Wang <jerryw@gmail.com>
2025-09-19 11:33:48 -0400

    fix up sketchybar

--- sketchybar/items/focus.sh
+++ sketchybar/items/focus.sh
@@ -2,16 +2,6 @@
 
 FRONT_APP_SCRIPT='sketchybar --set $NAME label="$INFO"'
 
-YABAI=(
-  script="$PLUGIN_DIR/yabai.sh"
-  label.drawing=off
-  icon.width=30
-  icon=$YABAI_GRID
-  icon.color=$ORANGE
-  updates=on
-  associated_display=active
-)
-
 FOCUS=(
   script="$FRONT_APP_SCRIPT"
   icon.drawing=off
@@ -23,10 +13,7 @@ FOCUS=(
 sketchybar --add event window_focus
 sketchybar --add event windows_on_spaces
 
-sketchybar --set yabai "${YABAI[@]}"
-sketchybar --subscribe yabai window_focus windows_on_spaces mouse.clicked
-
-sketchybar --add item focus left
+sketchybar --add item focus center
 sketchybar --set focus "${FOCUS[@]}"
 sketchybar --subscribe focus front_app_switched
 
--- sketchybar/items/spaces.sh
+++ sketchybar/items/spaces.sh
@@ -1,38 +1,19 @@
 #!/usr/bin/env sh
 
-SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15")
+sketchybar --add event aerospace_workspace_change
 
-# Destroy space on right click, focus space on left click.
-# New space by left clicking separator (>)
-SPACE_CLICK_SCRIPT='[ "$BUTTON" = "right" ] && (yabai -m space --destroy $SID; sketchybar --trigger space_change) || yabai -m space --focus $SID 2>/dev/null'
-
-SPACE=(
-  icon.padding_left=18
-  icon.padding_right=18
-  label.padding_right=25
-  icon.color=$WHITE
-  icon.font="$FONT:Bold:12.0"
-  icon.highlight_color=$GREEN
-  background.padding_left=-3
-  background.padding_right=-3
-  background.color=$BG_SEC_COLR
-  background.drawing=on
-  label.background.height=26
-  label.background.drawing=on
-  label.background.color=$BG_SEC_COLR
-  label.background.corner_radius=9
-  label.drawing=off
-  script="$PLUGIN_DIR/space.sh"
-  click_script="$SPACE_CLICK_SCRIPT"
-)
-
-sid=0
-for i in "${!SPACE_ICONS[@]}"
-do
-  sid=$(($i+1))
-  sketchybar --add space space.$sid left
-  sketchybar --set space.$sid associated_space=$sid
-  sketchybar --set space.$sid icon=${SPACE_ICONS[i]}
-  sketchybar --set space.$sid "${SPACE[@]}"
+for monitor in $(aerospace list-monitors | awk '{print $1}'); do
+    for sid in $(aerospace list-workspaces --monitor $monitor); do
+        sketchybar --add item space.$sid left \
+            --subscribe space.$sid aerospace_workspace_change \
+            --set space.$sid \
+            display=$monitor \
+            background.color=0x44ffffff \
+            background.corner_radius=5 \
+            background.height=20 \
+            background.drawing=off \
+            label="$sid" \
+            click_script="aerospace workspace $sid" \
+            script="$CONFIG_DIR/plugins/aerospace.sh $sid"
+    done
 done
-
