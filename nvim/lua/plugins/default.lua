953762cbaea0cf9133f2a5c9ec55d5a0845f2f48
Jerry Wang <jerryw@gmail.com>
2025-09-22 10:44:33 -0400

    fix up aerospace config

--- nvim/lua/plugins/default.lua
+++ nvim/lua/plugins/default.lua
@@ -27,7 +27,7 @@ return {
   {
     "akinsho/bufferline.nvim",
     init = function()
-      local bufline = require("catppuccin.groups.integrations.bufferline")
+      local bufline = require("catppuccin.special.bufferline")
       function bufline.get()
         return bufline.get_theme()
       end
