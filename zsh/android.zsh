014fc11fd89ce675d5a7cdb79ae0b3b4a9b36fbf
Jerry Wang <jerryw@gmail.com>
2025-10-31 12:37:41 -0400

    set android_home

---
+++ zsh/android.zsh
@@ -0,0 +1,4 @@
+export ANDROID_HOME=$HOME/Android/sdk
+export PATH=$ANDROID_HOME/cmdline-tools/tools/bin/:$PATH
+export PATH=$ANDROID_HOME/emulator/:$PATH
+export PATH=$ANDROID_HOME/platform-tools/:$PATH
