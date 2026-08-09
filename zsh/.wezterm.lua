387d19c4d91d5474a1053eb49cd5a018ea667888
Jerry Wang <jerryw@gmail.com>
2026-08-09 13:33:33 -0400

    added wezterm

---
+++ zsh/.wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 13
+config.font = wezterm.font("FiraMono Nerd Font")
+config.color_scheme = "Catppuccin Mocha"
+
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
